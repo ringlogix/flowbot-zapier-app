6c0cde8a9b333a9dd00a5062e97127932aec8f2f
Anton Shabinskiy <anton@ringlogix.com>
2025-12-16 15:31:00 +0100

    Fix package paths and add index.ts

---
+++ index.ts
@@ -0,0 +1,6 @@
+// Export all nodes for n8n
+export * from './nodes/CreateTicketTrigger/CreateTicketTrigger.node';
+export * from './nodes/UpdateTicketTrigger/UpdateTicketTrigger.node';
+export * from './nodes/StartTypingSoundAction/StartTypingSoundAction.node';
+export * from './nodes/StopTypingSoundAction/StopTypingSoundAction.node';
+export * from './CallTransferAction/CallTransferAction.node';
